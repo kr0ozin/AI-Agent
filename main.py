bb94bffb28d08f5cd6481ad83002b4868068d34c
kroozin <hourigan.brian@gmail.com>
2025-08-19 14:19:13 -0500

    Added Verbose

--- main.py
+++ main.py
@@ -3,8 +3,15 @@ from dotenv import load_dotenv
 from google import genai
 from google.genai import types
 import sys
+import argparse
 
 def main():
+    parser = argparse.ArgumentParser(description="Basic AI Agent")
+    parser.add_argument("user_prompt")
+    parser.add_argument("--verbose", "-v", action="store_true")
+
+    args = parser.parse_args()
+
     load_dotenv()
     api_key = os.environ.get("GEMINI_API_KEY")
     client = genai.Client(api_key=api_key)
@@ -18,9 +25,13 @@ def main():
     response = client.models.generate_content(
         model='gemini-2.0-flash-001', contents=messages,
     )
+
+    if args.verbose:
+        print(f"User prompt: {sys.argv[1]}")
+        print("Prompt tokens: " + str(response.usage_metadata.prompt_token_count))
+        print("Response tokens: " + str(response.usage_metadata.candidates_token_count))
+
     print(response.text)
-    print("Prompt tokens: " + str(response.usage_metadata.prompt_token_count))
-    print("Response tokens: " + str(response.usage_metadata.candidates_token_count))
 
 
 
